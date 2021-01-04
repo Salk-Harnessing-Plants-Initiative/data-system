2892820ae40569fe05acc358f86a40149749bc01
Russell Tran <tranrl@stanford.edu>
2021-01-03 21:46:18 -0800

    Copy in some example psychopg2 code

--- src/aws-lambda/process-image-upload/app.py
+++ src/aws-lambda/process-image-upload/app.py
@@ -1,3 +1,4 @@
+import os
 import json
 from pyzbar.pyzbar import decode
 from PIL import Image
@@ -10,3 +11,34 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
+
+
+import psycopg2
+from psycopg2 import Error
+
+try:
+    # Connect to an existing database
+    connection = psycopg2.connect(user=os.environ['user'],
+                                  password=os.environ['password'],
+                                  host=os.environ['host'],
+                                  port=os.environ['port'],
+                                  database=os.environ['database'])
+
+    # Create a cursor to perform database operations
+    cursor = connection.cursor()
+    # Print PostgreSQL details
+    print("PostgreSQL server information")
+    print(connection.get_dsn_parameters(), "\n")
+    # Executing a SQL query
+    cursor.execute("SELECT version();")
+    # Fetch result
+    record = cursor.fetchone()
+    print("You are connected to - ", record, "\n")
+
+except (Exception, Error) as error:
+    print("Error while connecting to PostgreSQL", error)
+finally:
+    if (connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
