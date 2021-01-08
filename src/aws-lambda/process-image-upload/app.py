e544fe75f23590ad0bdce10c79208ffbfe852906
Russell Tran <tranrl@stanford.edu>
2021-01-07 23:31:21 -0800

    Create abstractions for steps (WIP)

--- src/aws-lambda/process-image-upload/app.py
+++ src/aws-lambda/process-image-upload/app.py
@@ -2,8 +2,40 @@ import os
 import json
 from pyzbar.pyzbar import decode
 from PIL import Image
+from io import StringIO
 
 def lambda_handler(event, context):
+    for record in event['Records']:
+        try:
+            process(record)
+        except:
+            pass
+
+def process(record):
+    bucket = record['s3']['bucket']['name']
+    image_key = record['s3']['object']['key']
+    if not image_key_valid(image_key):
+        return
+    image = download_image(event)
+    container_id = decode_qr(image)
+    timestamp = get_timestamp(image_key, image)
+    user_input_filename = get_user_input_filename(image_key)
+    thumbnail = generate_thumbnail(image)
+    thumbnail_key = create_thumbnail_key(image_key)
+    upload_thumbnail(bucket, thumbnail_key, thumbnail)
+    insert_into_database(image_key=image_key, container_id=container_id, timestamp=timestamp,
+        user_input_filename=user_input_filename, thumbnail_key=thumbnail_key)
+
+def image_key_valid(image_key):
+    with open('config.json') as f:
+        config = json.load(f)
+        return accepted_directory(config, image_key) and accepted_filetype(config, image_key)
+
+def accepted_directory(image_key):
+
+
+# user_input_filename should come from s3 metadata
+
     # TODO implement
     print(decode(Image.open("taco.png")))
     print(decode(Image.open("russell_bacon_0.tif")))
