064a5a0604b78b4c135401d9483574bec5c0d80e
Russell Tran <tranrl@stanford.edu>
2021-01-21 23:41:06 -0800

    Update postgres

--- src/postgres/create.sql
+++ src/postgres/create.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 12.4
 -- Dumped by pg_dump version 13.0
 
--- Started on 2021-01-15 01:12:49 PST
+-- Started on 2021-01-21 23:39:40 PST
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -49,7 +49,9 @@ CREATE TABLE public.experiment (
     scientist character varying NOT NULL,
     created_at timestamp with time zone DEFAULT now() NOT NULL,
     description character varying,
-    created_by character varying
+    created_by character varying,
+    box_image_folder_id character varying,
+    box_csv_folder_id character varying
 );
 
 
@@ -63,14 +65,15 @@ ALTER TABLE public.experiment OWNER TO postgres;
 CREATE TABLE public.greenhouse_box (
     section_name character varying NOT NULL,
     experiment_id character varying NOT NULL,
-    box_folder_id character varying
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    created_by character varying
 );
 
 
 ALTER TABLE public.greenhouse_box OWNER TO postgres;
 
 --
--- TOC entry 3901 (class 0 OID 0)
+-- TOC entry 3914 (class 0 OID 0)
 -- Dependencies: 208
 -- Name: TABLE greenhouse_box; Type: COMMENT; Schema: public; Owner: postgres
 --
@@ -94,7 +97,7 @@ CREATE TABLE public.greenhouse_giraffe (
 ALTER TABLE public.greenhouse_giraffe OWNER TO postgres;
 
 --
--- TOC entry 3902 (class 0 OID 0)
+-- TOC entry 3916 (class 0 OID 0)
 -- Dependencies: 209
 -- Name: TABLE greenhouse_giraffe; Type: COMMENT; Schema: public; Owner: postgres
 --
@@ -167,13 +170,33 @@ CREATE TABLE public.section (
 ALTER TABLE public.section OWNER TO postgres;
 
 --
--- TOC entry 3903 (class 0 OID 0)
+-- TOC entry 3921 (class 0 OID 0)
 -- Dependencies: 210
 -- Name: TABLE section; Type: COMMENT; Schema: public; Owner: postgres
 --
 
 COMMENT ON TABLE public.section IS 'As in section of a greenhouse or section of a crop field';
 
+
+--
+-- TOC entry 211 (class 1259 OID 16812)
+-- Name: section_environment; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.section_environment (
+    section_name character varying NOT NULL,
+    leach_ph numeric,
+    leach_ec_ms_cm numeric,
+    experiment_id character varying NOT NULL,
+    environment_timestamp timestamp with time zone NOT NULL,
+    emitter_volume_ml numeric,
+    emitter_ph numeric,
+    emitter_ec_ms_cm numeric,
+    leach_volume_ml numeric
+);
+
+
+ALTER TABLE public.section_environment OWNER TO postgres;
 
 --
 -- TOC entry 205 (class 1259 OID 16504)
@@ -190,7 +213,7 @@ CREATE TABLE public.species (
 ALTER TABLE public.species OWNER TO postgres;
 
 --
--- TOC entry 3740 (class 2606 OID 16642)
+-- TOC entry 3747 (class 2606 OID 16642)
 -- Name: container container_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -199,7 +222,7 @@ ALTER TABLE ONLY public.container
 
 
 --
--- TOC entry 3728 (class 2606 OID 16646)
+-- TOC entry 3735 (class 2606 OID 16646)
 -- Name: plant containing_constraint; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -208,7 +231,7 @@ ALTER TABLE ONLY public.plant
 
 
 --
--- TOC entry 3736 (class 2606 OID 16424)
+-- TOC entry 3743 (class 2606 OID 16424)
 -- Name: experiment experiment_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -217,7 +240,7 @@ ALTER TABLE ONLY public.experiment
 
 
 --
--- TOC entry 3750 (class 2606 OID 16707)
+-- TOC entry 3757 (class 2606 OID 16707)
 -- Name: greenhouse_box greenhouse_box_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -226,7 +249,7 @@ ALTER TABLE ONLY public.greenhouse_box
 
 
 --
--- TOC entry 3754 (class 2606 OID 16715)
+-- TOC entry 3759 (class 2606 OID 16715)
 -- Name: greenhouse_giraffe greenhouse_giraffe_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -235,7 +258,7 @@ ALTER TABLE ONLY public.greenhouse_giraffe
 
 
 --
--- TOC entry 3747 (class 2606 OID 16555)
+-- TOC entry 3754 (class 2606 OID 16555)
 -- Name: image image_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -244,7 +267,7 @@ ALTER TABLE ONLY public.image
 
 
 --
--- TOC entry 3756 (class 2606 OID 16717)
+-- TOC entry 3761 (class 2606 OID 16717)
 -- Name: greenhouse_giraffe key_template_unique; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -253,7 +276,7 @@ ALTER TABLE ONLY public.greenhouse_giraffe
 
 
 --
--- TOC entry 3732 (class 2606 OID 16473)
+-- TOC entry 3739 (class 2606 OID 16473)
 -- Name: plant local_id_constraint; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -262,7 +285,7 @@ ALTER TABLE ONLY public.plant
 
 
 --
--- TOC entry 3734 (class 2606 OID 16644)
+-- TOC entry 3741 (class 2606 OID 16644)
 -- Name: plant plant_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -271,7 +294,7 @@ ALTER TABLE ONLY public.plant
 
 
 --
--- TOC entry 3745 (class 2606 OID 16520)
+-- TOC entry 3752 (class 2606 OID 16520)
 -- Name: scientist scientist_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -280,7 +303,16 @@ ALTER TABLE ONLY public.scientist
 
 
 --
--- TOC entry 3758 (class 2606 OID 16759)
+-- TOC entry 3769 (class 2606 OID 16819)
+-- Name: section_environment section_environment_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.section_environment
+    ADD CONSTRAINT section_environment_pkey PRIMARY KEY (experiment_id, section_name, environment_timestamp);
+
+
+--
+-- TOC entry 3763 (class 2606 OID 16759)
 -- Name: section section_id_unique; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -289,16 +321,7 @@ ALTER TABLE ONLY public.section
 
 
 --
--- TOC entry 3752 (class 2606 OID 16778)
--- Name: greenhouse_box section_name_box_folder_unique; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.greenhouse_box
-    ADD CONSTRAINT section_name_box_folder_unique UNIQUE (section_name, box_folder_id);
-
-
---
--- TOC entry 3760 (class 2606 OID 16757)
+-- TOC entry 3765 (class 2606 OID 16757)
 -- Name: section section_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -307,7 +330,7 @@ ALTER TABLE ONLY public.section
 
 
 --
--- TOC entry 3743 (class 2606 OID 16511)
+-- TOC entry 3750 (class 2606 OID 16511)
 -- Name: species species_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -316,7 +339,7 @@ ALTER TABLE ONLY public.species
 
 
 --
--- TOC entry 3729 (class 1259 OID 16652)
+-- TOC entry 3736 (class 1259 OID 16652)
 -- Name: fki_container_fkey; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -324,7 +347,7 @@ CREATE INDEX fki_container_fkey ON public.plant USING btree (container_id);
 
 
 --
--- TOC entry 3730 (class 1259 OID 16435)
+-- TOC entry 3737 (class 1259 OID 16435)
 -- Name: fki_experiment-foreign-key; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -332,7 +355,7 @@ CREATE INDEX "fki_experiment-foreign-key" ON public.plant USING btree (experimen
 
 
 --
--- TOC entry 3741 (class 1259 OID 16543)
+-- TOC entry 3748 (class 1259 OID 16543)
 -- Name: fki_experiment_fkey; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -340,7 +363,15 @@ CREATE INDEX fki_experiment_fkey ON public.container USING btree (experiment_id)
 
 
 --
--- TOC entry 3737 (class 1259 OID 16534)
+-- TOC entry 3766 (class 1259 OID 16831)
+-- Name: fki_experiment_id_fkey; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX fki_experiment_id_fkey ON public.section_environment USING btree (experiment_id);
+
+
+--
+-- TOC entry 3744 (class 1259 OID 16534)
 -- Name: fki_scientist_fkey; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -348,7 +379,7 @@ CREATE INDEX fki_scientist_fkey ON public.experiment USING btree (scientist);
 
 
 --
--- TOC entry 3748 (class 1259 OID 16736)
+-- TOC entry 3755 (class 1259 OID 16736)
 -- Name: fki_section_fkey; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -356,7 +387,15 @@ CREATE INDEX fki_section_fkey ON public.greenhouse_box USING btree (section_name
 
 
 --
--- TOC entry 3738 (class 1259 OID 16528)
+-- TOC entry 3767 (class 1259 OID 16825)
+-- Name: fki_section_name_fkey; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX fki_section_name_fkey ON public.section_environment USING btree (section_name);
+
+
+--
+-- TOC entry 3745 (class 1259 OID 16528)
 -- Name: fki_species_fkey; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -364,7 +403,7 @@ CREATE INDEX fki_species_fkey ON public.experiment USING btree (species);
 
 
 --
--- TOC entry 3762 (class 2606 OID 16647)
+-- TOC entry 3771 (class 2606 OID 16647)
 -- Name: plant container_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -373,7 +412,7 @@ ALTER TABLE ONLY public.plant
 
 
 --
--- TOC entry 3761 (class 2606 OID 16436)
+-- TOC entry 3770 (class 2606 OID 16436)
 -- Name: plant experiment_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -382,7 +421,7 @@ ALTER TABLE ONLY public.plant
 
 
 --
--- TOC entry 3765 (class 2606 OID 16538)
+-- TOC entry 3774 (class 2606 OID 16538)
 -- Name: container experiment_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -391,7 +430,7 @@ ALTER TABLE ONLY public.container
 
 
 --
--- TOC entry 3766 (class 2606 OID 16745)
+-- TOC entry 3775 (class 2606 OID 16745)
 -- Name: greenhouse_box experiment_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -400,7 +439,7 @@ ALTER TABLE ONLY public.greenhouse_box
 
 
 --
--- TOC entry 3768 (class 2606 OID 16718)
+-- TOC entry 3777 (class 2606 OID 16718)
 -- Name: greenhouse_giraffe experiment_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -409,7 +448,16 @@ ALTER TABLE ONLY public.greenhouse_giraffe
 
 
 --
--- TOC entry 3764 (class 2606 OID 16529)
+-- TOC entry 3779 (class 2606 OID 16826)
+-- Name: section_environment experiment_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.section_environment
+    ADD CONSTRAINT experiment_id_fkey FOREIGN KEY (experiment_id) REFERENCES public.experiment(experiment_id) NOT VALID;
+
+
+--
+-- TOC entry 3773 (class 2606 OID 16529)
 -- Name: experiment scientist_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -418,7 +466,7 @@ ALTER TABLE ONLY public.experiment
 
 
 --
--- TOC entry 3767 (class 2606 OID 16760)
+-- TOC entry 3776 (class 2606 OID 16760)
 -- Name: greenhouse_box section_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -427,7 +475,16 @@ ALTER TABLE ONLY public.greenhouse_box
 
 
 --
--- TOC entry 3763 (class 2606 OID 16523)
+-- TOC entry 3778 (class 2606 OID 16820)
+-- Name: section_environment section_name_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.section_environment
+    ADD CONSTRAINT section_name_fkey FOREIGN KEY (section_name) REFERENCES public.section(section_name) NOT VALID;
+
+
+--
+-- TOC entry 3772 (class 2606 OID 16523)
 -- Name: experiment species_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -436,7 +493,7 @@ ALTER TABLE ONLY public.experiment
 
 
 --
--- TOC entry 3900 (class 0 OID 0)
+-- TOC entry 3911 (class 0 OID 0)
 -- Dependencies: 3
 -- Name: SCHEMA public; Type: ACL; Schema: -; Owner: postgres
 --
@@ -445,9 +502,109 @@ REVOKE ALL ON SCHEMA public FROM rdsadmin;
 REVOKE ALL ON SCHEMA public FROM PUBLIC;
 GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
+GRANT USAGE ON SCHEMA public TO readaccess;
 
 
--- Completed on 2021-01-15 01:12:54 PST
+--
+-- TOC entry 3912 (class 0 OID 0)
+-- Dependencies: 204
+-- Name: TABLE container; Type: ACL; Schema: public; Owner: postgres
+--
+
+GRANT SELECT ON TABLE public.container TO readaccess;
+
+
+--
+-- TOC entry 3913 (class 0 OID 0)
+-- Dependencies: 203
+-- Name: TABLE experiment; Type: ACL; Schema: public; Owner: postgres
+--
+
+GRANT SELECT ON TABLE public.experiment TO readaccess;
+
+
+--
+-- TOC entry 3915 (class 0 OID 0)
+-- Dependencies: 208
+-- Name: TABLE greenhouse_box; Type: ACL; Schema: public; Owner: postgres
+--
+
+GRANT SELECT ON TABLE public.greenhouse_box TO readaccess;
+
+
+--
+-- TOC entry 3917 (class 0 OID 0)
+-- Dependencies: 209
+-- Name: TABLE greenhouse_giraffe; Type: ACL; Schema: public; Owner: postgres
+--
+
+GRANT SELECT ON TABLE public.greenhouse_giraffe TO readaccess;
+
+
+--
+-- TOC entry 3918 (class 0 OID 0)
+-- Dependencies: 207
+-- Name: TABLE image; Type: ACL; Schema: public; Owner: postgres
+--
+
+GRANT SELECT ON TABLE public.image TO readaccess;
+
+
+--
+-- TOC entry 3919 (class 0 OID 0)
+-- Dependencies: 202
+-- Name: TABLE plant; Type: ACL; Schema: public; Owner: postgres
+--
+
+GRANT SELECT ON TABLE public.plant TO readaccess;
+
+
+--
+-- TOC entry 3920 (class 0 OID 0)
+-- Dependencies: 206
+-- Name: TABLE scientist; Type: ACL; Schema: public; Owner: postgres
+--
+
+GRANT SELECT ON TABLE public.scientist TO readaccess;
+
+
+--
+-- TOC entry 3922 (class 0 OID 0)
+-- Dependencies: 210
+-- Name: TABLE section; Type: ACL; Schema: public; Owner: postgres
+--
+
+GRANT SELECT ON TABLE public.section TO readaccess;
+
+
+--
+-- TOC entry 3923 (class 0 OID 0)
+-- Dependencies: 211
+-- Name: TABLE section_environment; Type: ACL; Schema: public; Owner: postgres
+--
+
+GRANT SELECT ON TABLE public.section_environment TO readaccess;
+
+
+--
+-- TOC entry 3924 (class 0 OID 0)
+-- Dependencies: 205
+-- Name: TABLE species; Type: ACL; Schema: public; Owner: postgres
+--
+
+GRANT SELECT ON TABLE public.species TO readaccess;
+
+
+--
+-- TOC entry 1733 (class 826 OID 16780)
+-- Name: DEFAULT PRIVILEGES FOR TABLES; Type: DEFAULT ACL; Schema: public; Owner: postgres
+--
+
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public REVOKE ALL ON TABLES  FROM postgres;
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT SELECT ON TABLES  TO readaccess;
+
+
+-- Completed on 2021-01-21 23:39:46 PST
 
 --
 -- PostgreSQL database dump complete
