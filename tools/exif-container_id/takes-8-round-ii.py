bb5ccca8339f10df6ff9bf183bb7e887adaeb45f
Russell Tran <tranrl@stanford.edu>
2021-02-02 17:09:27 -0800

    Spun off tools to https://github.com/Salk-Harnessing-Plants-Initiative/data-system-tools

--- tools/exif-container_id/takes-8-round-ii.py
+++
@@ -1,7 +0,0 @@
-import exifcontainer
-exifcontainer.run(
-    "/raidix/groot-data/russell_tran/real_data/migrations/takes-8-round-ii/takes-8-round-ii-new-plate-2021-01-03-000156.csv", 
-    "/raidix/groot-data/russell_tran/real_data/migrations/takes-8-round-ii/directory.json",
-    exifcontainer.container_id_data,
-    output_log="/raidix/groot-data/russell_tran/real_data/migrations/takes-8-round-ii/log.txt"
-)
