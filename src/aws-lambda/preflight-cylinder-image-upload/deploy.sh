51c6d2f629a0ff6af55956b95f59e2d86edca161
Russell Tran <tranrl@stanford.edu>
2021-02-09 16:05:21 -0800

    First version of preflight-cylinder-image-upload

---
+++ src/aws-lambda/preflight-cylinder-image-upload/deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+GOOS=linux go build main.go
+zip -r lambdaFunc.zip .
+aws lambda update-function-code --function-name preflight-cylinder-image-upload --zip-file fileb:///Users/russelltran/Documents/code/salk-hpi/data-system/src/aws-lambda/preflight-cylinder-image-upload/lambdaFunc.zip
---
+++ src/aws-lambda/preflight-cylinder-image-upload/main.go
@@ -0,0 +1,58 @@
+package main
+ 
+import (
+        "fmt"
+        "log"
+        "os"
+        "database/sql"
+        "github.com/aws/aws-lambda-go/lambda"
+
+        _ "github.com/lib/pq"
+)
+
+var (
+	host string    = os.Getenv("host")
+	port string    = os.Getenv("port")
+	user string    = os.Getenv("user")
+	password string = os.Getenv("password")
+	database string = os.Getenv("database")
+)
+var db *sql.DB
+
+type MyEvent struct {
+    QrCode string `json:"qr_code"`
+    UploadDeviceId string `json:"upload_device_id"`
+}
+ 
+type MyResponse struct {
+	QrCodeValid bool `json:"qr_code_valid:"`
+}
+
+func CheckError(err error) {
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
+func init() {
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s "+
+    	"password=%s dbname=%s sslmode=disable",
+    	host, port, user, password, database)
+    var err error
+	db, err = sql.Open("postgres", psqlconn)
+	CheckError(err)
+}
+
+func HandleLambdaEvent(event MyEvent) (MyResponse, error) {
+	var count int
+	row := db.QueryRow(`SELECT COUNT(*) FROM "plant" WHERE "plant_id" = $1 OR "container_id" = $1`, event.QrCode)
+	err := row.Scan(&count)
+	CheckError(err)
+
+	response := MyResponse{(count > 0)}
+    return response, nil
+}
+ 
+func main() {
+        lambda.Start(HandleLambdaEvent)
+}
