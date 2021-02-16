651f4b967e0d953861fa337a94d8fff2c45af0b1
Russell Tran <tranrl@stanford.edu>
2021-02-15 17:57:01 -0800

    Quit bash if any command fails

--- src/aws-lambda/preflight-cylinder-image-upload/deploy.sh
+++ src/aws-lambda/preflight-cylinder-image-upload/deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
-GOOS=linux go build main.go
+#!/bin/bash -e
+GOOS=linux GOARCH=amd64 go build main.go
 zip -r lambdaFunc.zip .
 aws lambda update-function-code --function-name preflight-cylinder-image-upload --zip-file fileb:///Users/russelltran/Documents/code/salk-hpi/data-system/src/aws-lambda/preflight-cylinder-image-upload/lambdaFunc.zip
