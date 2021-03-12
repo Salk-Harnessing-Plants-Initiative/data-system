cf566a1d5e84d92bbe0946a79f96e5ebfcc4a09c
Russell Tran <tranrl@stanford.edu>
2021-03-11 17:30:24 -0800

    Combine deploy shell scripts

--- src/aws-lambda/register-plants/deploy.sh
+++ src/aws-lambda/register-plants/deploy.sh
@@ -1,3 +1,7 @@
-#!/bin/bash
+#!/bin/bash -e
 zip -r lambdaFunc.zip .
-aws lambda update-function-code --function-name register-plants --zip-file fileb:///Users/russelltran/Documents/code/salk-hpi/data-system/src/aws-lambda/register-plants/lambdaFunc.zip
+if [[ $1 == 'prod' ]]; then 
+	aws lambda update-function-code --function-name register-plants --zip-file fileb:///Users/russelltran/Documents/code/salk-hpi/data-system/src/aws-lambda/register-plants/lambdaFunc.zip
+else
+	aws lambda update-function-code --function-name dev-register-plants --zip-file fileb:///Users/russelltran/Documents/code/salk-hpi/data-system/src/aws-lambda/register-plants/lambdaFunc.zip
+fi
